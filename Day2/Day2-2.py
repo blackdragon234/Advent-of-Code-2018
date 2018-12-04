f799f51392658bbedfe14102edf804929337f724
blackdragon234 <44242479+blackdragon234@users.noreply.github.com>
2018-12-04 15:51:45 +0100

    Day 2-2 flawed solution
    Solved but the currently solution is scrambled set of chars of the string instead of the string with the characters removed

--- Day2/Day2-2.py
+++ Day2/Day2-2.py
@@ -1,25 +1,15 @@
 import os
 os.chdir('Day2')
 from collections import Counter
-
-data = open('test2.txt','r')
+answer = []
+data = open('input.txt','r')
 
 for line in data:
-    baseline = line
-    nextline = next(data)
-    result = first_difference(baseline, nextline)
-    for c in line:
-        d[c] +=  1
-    if 2 in d.values():
-        duplicate += 1
-    if 3 in d.values():
-        triplet += 1
-
-def first_difference(str1, str2):
-    for a, b in zip(str1, str2):
-        if a != b:
-            return a+b
-#        if baseline almost = line then position = data.tell()
-print (duplicate, triplet)
-checksum = duplicate * triplet
-print (checksum)
+    baseline = set(line)
+    for otherlines in data:
+        nextline = set(otherlines)
+        result = set.difference(nextline, baseline)
+        if len(result) == 2:
+            answer.append(nextline-result)
+            answer.append(baseline-result)
+print (','.join(answer[1]))

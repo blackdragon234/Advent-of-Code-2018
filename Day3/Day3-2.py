fd01e96aeee05336f085d31d28ddf8120a16f3eb
blackdragon234 <44242479+blackdragon234@users.noreply.github.com>
2018-12-06 16:30:58 +0100

    Solved 3-2

--- Day3/Day3-2.py
+++ Day3/Day3-2.py
@@ -2,37 +2,56 @@ import os
 os.chdir('Day3')
 from collections import Counter
 
-coordinateslist = Counter()
-answer = set([])
-duplicates = 0
+testlist = Counter()
 
 #       splits the input in relevant parts. ID, @, XYcoordinates and size.
-def makeanindex ()
+def readthelines ():
+        coordinateslist = Counter()
+        f = open('input.txt')
+        for line in f:
+                index = line.split(" ")
+                rectangleSize = index[3].split("x")
+                rectangleCoordinates = index[2].split(",")
+                rectangleX = int(rectangleCoordinates[0])
+                rectangleY = int(rectangleCoordinates[1][0:-1])
+                rectangleW = int(rectangleSize[0]) 
+                rectangleH = int(rectangleSize[1])
+                while rectangleH > 0:
+                        Ycoordinate = rectangleY + rectangleH - 1
+                        while rectangleW > 0:
+                                Xcoordinate = rectangleX + rectangleW - 1      
+                                XYcoordinate = str(Ycoordinate)+ '-' + str(Xcoordinate)
+                                coordinateslist[XYcoordinate] += 1
+                                rectangleW -= 1
+                        rectangleH -= 1
+                        rectangleW = int(rectangleSize[0])
+        return coordinateslist
+
+testlist = readthelines()
+answer = False
+
 f = open('input.txt')
 for line in f:
-        index = line.split(" ")
-        answer.add(index[0])
-        rectangleID = index[0][1:]
-        rectangleSize = index[3].split("x")
-        rectangleCoordinates = index[2].split(",")
-        rectangleX = int(rectangleCoordinates[0])
-        rectangleY = int(rectangleCoordinates[1][0:-1])
-        rectangleW = int(rectangleSize[0]) 
-        rectangleH = int(rectangleSize[1])
-
-#       counts the squares of the rectangle and maps them to coordinates
-#       if a coordinate is already 1 then the ID is cleared from the answer string
-        while rectangleH > 0:
-                Ycoordinate = rectangleY + rectangleH - 1
-                while rectangleW > 0:
-                        Xcoordinate = rectangleX + rectangleW - 1      
-                        XYcoordinate = str(Ycoordinate)+ '-' + str(Xcoordinate)
-                        if coordinateslist[XYcoordinate]  == 1:
-                                coordinateslist[XYcoordinate] += 1
-                                answer.discard(rectangleID)
-                        else:
-                                coordinateslist[XYcoordinate] += 1 
-                        rectangleW -= 1
-                rectangleH -= 1
-                rectangleW = int(rectangleSize[0])
-print (answer)
+        if answer == True:
+                print (rectangleID)
+                break
+        else:
+                answer = True
+                index = line.split(" ")
+                rectangleID = index[0][1:]
+                rectangleSize = index[3].split("x")
+                rectangleCoordinates = index[2].split(",")
+                rectangleX = int(rectangleCoordinates[0])
+                rectangleY = int(rectangleCoordinates[1][0:-1])
+                rectangleW = int(rectangleSize[0]) 
+                rectangleH = int(rectangleSize[1])
+                while rectangleH > 0 and answer:
+                        Ycoordinate = rectangleY + rectangleH - 1
+                        while rectangleW > 0 and answer:
+                                Xcoordinate = rectangleX + rectangleW - 1
+                                XYcoordinate = str(Ycoordinate)+ '-' + str(Xcoordinate)
+                                if testlist[XYcoordinate] != 1:
+                                        answer = False
+                                rectangleW -= 1
+                        rectangleH -= 1
+                        rectangleW = int(rectangleSize[0])
